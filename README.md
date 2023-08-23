3c975cd661e932352f968ac25ec6ecfe998f77e0
Gustavo Lima <gustavoblima@me.com>
2023-08-23 19:05:20 +0000

    Data Collection Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Template for Machine Learning projects
+# US Accidents - Finding the Causes of Accidents and How to Minimize them
+
+In this project the idea is to find a model to minimize the accidents in the US. 
--- src/01_DataCollection.py
+++ src/01_DataCollection.py
@@ -1,5 +1,7 @@
 # Before executing make sure you have your kaggle.json credential file under
 # ~/.kaggle/ folder and do a chmod 600 to kaggle.json for security reasons.
+# Alternatively Do a "export KAGGLE_CONFIG_DIR=/workspaces/cesargustavo-Final_Project/.kaggle" and 
+#"chmod 600 .kaggle/kaggle.json"
 # This is a mandatory step to be able to download the dataset in question.
 
 # Import Packages
